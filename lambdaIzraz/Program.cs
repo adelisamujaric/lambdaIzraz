7769ee9c47cd4f4c03c5a96a93609fb68965ad2c
caj.od.breskve@outlook.com <caj.od.breskve@outlook.com>
2019-12-29 22:20:35 +0100

    Add project files.

---
+++ lambdaIzraz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lambdaIzraz
+{
+    delegate double Circle(double r); //P=r*r*PI//
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle Operation = (double r) =>
+              {
+                  double PI = 3.14;
+                  double result = r * r * PI;
+                  return result;
+              };
+            Console.WriteLine("Result is: {0}", Operation(2));
+        }
+     
+    }
+}
